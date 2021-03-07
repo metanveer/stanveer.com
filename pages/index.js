3af55e3c2eaf3fd8b1b769dce2def164cda04ac9
Tanveer Hossain <stanveer.me@gmail.com>
2021-03-07 23:02:18 +0600

    remove under-construction page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function UnderConstruction() {
   return (
     <div className={styles.container}>
       <Head>
@@ -9,7 +9,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>Test</main>
+      <main className={styles.main}>
+        <h1 style={{ color: "red" }} className={styles.title}>
+          Website Under Construction!
+        </h1>
+        <img
+          src="/under-construction.svg"
+          alt="Page under construction"
+          className={styles.construction}
+        />
+      </main>
 
       <footer className={styles.footer}>
         &copy;2021 Tanveer Hossain | All rights reserved
